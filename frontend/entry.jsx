9949be80e7b96a032117de7bac8a0b1113ad96b5
Khalil Nasirov <khalil.nasirov@gmail.com>
2017-03-15 15:45:22 -0700

    persist current user

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import {login} from './actions/session_actions';
-const store = configureStore();
-window.store = store;
-window.login = login;
-
 document.addEventListener('DOMContentLoaded', () => {
+  let preloadedState = {};
+  if (window.currentUser) {
+    preloadedState = { session: { currentUser: window.currentUser } };
+  }
+  const store = configureStore(preloadedState);
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root)
 });
