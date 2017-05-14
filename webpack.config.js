b8482ff65681407ec6e55f9f9913bd760a72f398
Khalil Nasirov <khalil.nasirov@gmail.com>
2017-05-14 15:21:43 -0700

    add ruby version and change main paramenter in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
     loaders: [
       {
         test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015', 'react']
